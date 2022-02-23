d7e1f511e5e6ca83f1565944cb137a0cca60ad5d
Jonathan Guiang <jkguiang@gmail.com>
2022-02-22 16:40:30 -0800

    added dummy SENSE api for testing

---
+++ nonsense.py
@@ -0,0 +1,18 @@
+"""
+Name-Only Nonfunctional Software defined networking (SDN) for End-to-end Networked 
+Science at the Exascale 
+"""
+
+DUMMY_LINK = "127.0.0.1"
+
+def allocate_links(*args, **kwargs):
+    return
+
+def get_links(*args, **kwargs):
+    return DUMMY_LINK, DUMMY_LINK
+
+def update_links():
+    return
+
+def free_links(*args, **kwargs):
+    return
