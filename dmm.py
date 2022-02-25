4e34674be759654050ca2c7d5b6ee6a29c529604
Jonathan Guiang <jkguiang@gmail.com>
2022-02-25 09:25:30 -0800

    removed multiprocessing for now

--- dmm.py
+++ dmm.py
@@ -1,5 +1,5 @@
+import nonsense as sense
 import argparse
-import multiprocessing as mp
 from multiprocessing.connection import Listener
 
 class DMM:
@@ -8,8 +8,7 @@ class DMM:
         self.port = port
         with open(authkey_file, "rb") as f_in:
             self.authkey = f_in.read()
-        self.pool = mp.Pool(processes=n_workers)
-        self.lock = mp.Lock()
+        self.n_workers = n_workers
         self.cache = {}
 
     def start(self):
@@ -20,15 +19,12 @@ class DMM:
                 print("connection accepted from", listener.last_accepted)
                 daemon, payload = connection.recv()
                 if daemon == "PREPARER":
-                    pool.apply_async(self.preparer_handler, (payload,))
+                    self.preparer_handler(payload)
                 elif daemon == "SUBMITTER":
-                    # FIXME: currently, any request that expects a response breaks
-                    #        parallelism, because next listener.accept() is not allowed
-                    #        until connection.send(resp.get()) finishes
-                    resp = pool.apply_async(self.submitter_handler, (payload,))
-                    connection.send(resp.get())
+                    result = self.submitter_handler(payload)
+                    connection.send(result)
                 elif daemon == "FINISHER":
-                    pool.apply_async(self.finisher_handler, (payload,))
+                    self.finisher_handler(payload)
 
     def preparer_handler(self, payload):
         to_cache = {}
@@ -44,49 +40,43 @@ class DMM:
                 transfer_data.update(additional_transfer_data)
                 to_cache[priority][rse_pair_id] = transfer_data
         # Update cache
-        self.lock.aquire()
         self.cache.update(to_cache)
-        self.lock.release()
 
     def submitter_handler(self, payload):
         priority = payload.get("priority")
         rse_pair_id = payload.get("rse_pair_id")
         submitted_transfers = payload.get("submitted_transfers")
         # Fetch transfer metadata
-        transfer_data = cache[priority][rse_pair_id]
+        transfer_data = self.cache[priority][rse_pair_id]
         # Update counters
         transfer_data["waiting_transfers"] -= submitted_transfers
         transfer_data["active_transfers"] += submitted_transfers
         # Get dummy SENSE links
-        nonsense.allocate_links(transfer_data)
-        src_link, dst_link = nonsense.get_links(priority, rse_pair_id)
+        sense.allocate_links(transfer_data)
+        src_link, dst_link = sense.get_links(priority, rse_pair_id)
         transfer_data["sense_map"] = {
             transfer_data["source_rse_id"]: src_link,
             transfer_data["dest_rse_id"]: dst_link,
         }
         # Update cache
-        self.lock.aquire()
-        cache[priority][rse_pair_id].update(transfer_data)
-        self.lock.release()
+        self.cache[priority][rse_pair_id].update(transfer_data)
         return transfer_data["sense_map"]
 
     def finisher_handler(self, payload):
         for priority, updated_jobs in payload.items():
-            active_jobs = cache[priority]
+            active_jobs = self.cache[priority]
             for rse_pair_id, updated_data in updated_jobs.items():
                 transfer_data = active_jobs[rse_pair_id]
                 transfer_data["transferred_bytes"] += updated_data["transferred_bytes"]
                 transfer_data["active_transfers"] -= updated_data["finished_transfers"]
                 transfer_data["finished_transfers"] += updated_data["finished_transfers"]
                 if transfer_data["finished_transfers"] == transfer_data["total_transfers"]:
-                    nonsense.free_links(priority, rse_pair_id)
+                    sense.free_links(priority, rse_pair_id)
                     active_jobs.pop(rse_pair_id)
-            lock.aquire()
             if active_jobs == {}:
-                cache.delete(priority)
+                self.cache.delete(priority)
             else:
-                cache[priority] = active_jobs
-            lock.release()
+                self.cache[priority] = active_jobs
 
 if __name__ == "__main__":
     cli = argparse.ArgumentParser(description="Rucio-SENSE data movement manager")
