341606392a0d8c601606e68a36926022f2cd87ad
Jonathan Guiang <jkguiang@gmail.com>
2022-03-03 11:45:02 -0800

    added nonsense-o calls, updated other funcs

--- nonsense.py
+++ nonsense.py
@@ -1,4 +1,5 @@
 import json
+
 """
 Name-Only Nonfunctional Software defined networking (SDN) for End-to-end Networked 
 Science at the Exascale 
@@ -10,40 +11,46 @@ def get_ipv6_pool(uri):
     """
     GET /discover/URI/ipv6pool
     """
-    with open("templates/sense_discover_ipv6pool.json", "r") as f_in:
-        template = json.load(f_in)
-    return template["ipv6_subnet_pool"].split(",")
+    with open(f"nonsense-o/discover/{uri}/ipv6pool", "r") as f_in:
+        response = json.load(f_in)
+    ipv6_pool = response["routing"][0]["ipv6_subnet_pool"].split(",")
+    ipv6_pool = [DUMMY_LINK for ipv6 in ipv6_pool]
+    return ipv6_pool
 
 def get_uplink_capacity(uri):
     """
     GET /discover/URI/peers
     """
-    with open("templates/sense_discover_peers.json", "r") as f_in:
-        template = json.load(f_in)
-    return float(template["port_capacity"])
+    with open(f"nonsense-o/discover/{uri}/peers", "r") as f_in:
+        response = json.load(f_in)
+    return float(response["peer_points"][0]["port_capacity"])
 
 def get_uri(rse_name):
     """
     GET /discover/lookup/RSE_NAME
     """
-    with open("templates/sense_discover_lookup.json", "r") as f_in:
-        template = json.load(f_in)
-    return __get_rooturi(template["results"][0]["resource"])
+    with open(f"nonsense-o/discover/lookup/{rse_name}", "r") as f_in:
+        response = json.load(f_in)
+    return __get_rooturi(response["results"][0]["resource"])
 
 def __get_rooturi(full_uri):
     """
-    GET /discover/lookup/FULL_URL/rooturi
+    GET /discover/lookup/FULL_URI/rooturi
     """
-    return "urn:ogf:network:ultralight.org:2013"
+    with open(f"nonsense-o/discover/lookup/{full_uri}/rooturi", "r") as f_in:
+        response = f_in.read()
+        if "\n" in response:
+            response = response[:-1]
+    return response
 
-def get_theoretical_bandwidth(*args, **kwargs):
+def get_theoretical_bandwidth(profile_uuid):
+    return 10**15
+
+def build_link(profile_uuid, src_uri, dst_uri, src_ipv6, dst_ipv6, bandwidth):
+    return "dummy_instance_uuid"
+
+def reprovision_link(instance_uuid, new_bandwidth):
     return
 
-def build_link(*args, **kwargs):
-    return
-
-def reprovision_link(*args, **kwargs):
-    return
-
-def delete_link(*args, **kwargs):
+def delete_link(instance_uuid):
     return
