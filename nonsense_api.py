216ae0c0891504497f5fca01b1c9b6543d45c370
Jonathan Guiang <jkguiang@gmail.com>
2022-03-10 22:19:22 -0800

    simplified nonsense

--- nonsense_api.py
+++ nonsense_api.py
@@ -1,58 +1,59 @@
 import json
+import yaml
+import uuid
 
-"""
-Name-Only Nonfunctional Software defined networking (SDN) for End-to-end Networked 
-Science at the Exascale 
-"""
+PROFILE_UUID = "ddd1dec0-83ab-4d08-bca6-9a83334cd6db"
 
-PROFILE_UUID = "dummy_profile_uuid"
-
-DUMMY_LINK = "127.0.0.1"
+def good_response(response):
+    return len(response) == 0 or "ERROR" in response or "error" in response
 
 def get_ipv6_pool(uri):
+    """Return a list of IPv6 subnets at given site
+
+    Note: not fully supported by SENSE yet
     """
-    GET /discover/URI/ipv6pool
-    """
-    with open(f"nonsense-o/discover/{uri}/ipv6pool", "r") as f_in:
-        response = json.load(f_in)
-    ipv6_pool = response["routing"][0]["ipv6_subnet_pool"].split(",")
-    ipv6_pool = [DUMMY_LINK for ipv6 in ipv6_pool]
+    ipv6_pool = [
+        "fc00::0010/124", "fc00::0020/124", "fc00::0030/124", 
+        "fc00::0040/124", "fc00::0050/124"
+    ]
     return ipv6_pool
 
 def get_uplink_capacity(uri):
-    """
-    GET /discover/URI/peers
-    """
-    with open(f"nonsense-o/discover/{uri}/peers", "r") as f_in:
-        response = json.load(f_in)
-    return float(response["peer_points"][0]["port_capacity"])
+    """Return the maximum uplink capacity in Mb/s for a given site
 
-def get_uri(rse_name):
+    Notes: not fully supported by SENSE yet
     """
-    GET /discover/lookup/RSE_NAME
-    """
-    with open(f"nonsense-o/discover/lookup/{rse_name}", "r") as f_in:
-        response = json.load(f_in)
-    return __get_rooturi(response["results"][0]["resource"])
+    return 100000
+
+def get_uri(rse_name, full=True):
+    """Return the root SENSE URI for a given Rucio RSE"""
+    full_uri = f"urn:ogf:network:{rse_name.lower()}.foo:{rse_name}"
+    if full:
+        return full_uri
+    else:
+        return __get_rooturi(full_uri)
 
 def __get_rooturi(full_uri):
+    """Return the root SENSE URI for a given full SENSE URI"""
+    return full_uri.split(":")[0]
+
+def get_theoretical_bandwidth(src_uri, dst_uri, instance_uuid=PROFILE_UUID):
+    """Return the maximum theoretical bandwidth available between two sites
+
+    Note: not fully supported by SENSE yet
     """
-    GET /discover/lookup/FULL_URI/rooturi
-    """
-    with open(f"nonsense-o/discover/lookup/{full_uri}/rooturi", "r") as f_in:
-        response = f_in.read()
-        if "\n" in response:
-            response = response[:-1]
-    return response
+    return uuid.uuid4(), 100000000000
 
-def get_theoretical_bandwidth(src_uri, dst_uri):
-    return 10**15
-
-def build_link(src_uri, dst_uri, src_ipv6, dst_ipv6, bandwidth):
-    return "dummy_instance_uuid", 0
-
-def reprovision_link(instance_uuid, new_bandwidth):
+def create_link(src_uri, dst_uri, src_ipv6, dst_ipv6, bandwidth, 
+                instance_uuid=PROFILE_UUID, alias=""):
+    """Create a SENSE guaranteed-bandwidth link between two sites"""
     return
 
 def delete_link(instance_uuid):
+    """Delete a SENSE link"""
     return
+
+def reprovision_link(old_instance_uuid, src_uri, dst_uri, src_ipv6, dst_ipv6, 
+                     new_bandwidth, alias=""):
+    """Reprovision a SENSE link"""
+    return uuid.uuid4()
