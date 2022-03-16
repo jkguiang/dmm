ffe45d504cc71d3568c473be97f1368c0ec6b022
Jonathan Guiang <jkguiang@gmail.com>
2022-03-15 20:18:19 -0700

    added realistic runtimes

--- nonsense_api.py
+++ nonsense_api.py
@@ -1,8 +1,9 @@
 import json
-import yaml
+import time
 import uuid
+import yaml
 
-PROFILE_UUID = "ddd1dec0-83ab-4d08-bca6-9a83334cd6db"
+PROFILE_UUID = uuid.uuid4()
 
 def good_response(response):
     return len(response) == 0 or "ERROR" in response or "error" in response
@@ -12,6 +13,7 @@ def get_ipv6_pool(uri):
 
     Note: not fully supported by SENSE yet
     """
+    time.sleep(0.5)
     ipv6_pool = [
         "fc00::0010/124", "fc00::0020/124", "fc00::0030/124", 
         "fc00::0040/124", "fc00::0050/124"
@@ -23,10 +25,12 @@ def get_uplink_capacity(uri):
 
     Notes: not fully supported by SENSE yet
     """
+    time.sleep(1)
     return 100000
 
 def get_uri(rse_name, full=True):
     """Return the root SENSE URI for a given Rucio RSE"""
+    time.sleep(0.5)
     full_uri = f"urn:ogf:network:{rse_name.lower()}.foo:{rse_name}"
     if full:
         return full_uri
@@ -42,18 +46,22 @@ def get_theoretical_bandwidth(src_uri, dst_uri, instance_uuid=PROFILE_UUID):
 
     Note: not fully supported by SENSE yet
     """
+    time.sleep(1)
     return uuid.uuid4(), 100000000000
 
 def create_link(src_uri, dst_uri, src_ipv6, dst_ipv6, bandwidth, 
                 instance_uuid=PROFILE_UUID, alias=""):
     """Create a SENSE guaranteed-bandwidth link between two sites"""
+    time.sleep(60)
     return
 
 def delete_link(instance_uuid):
     """Delete a SENSE link"""
+    time.sleep(60)
     return
 
 def reprovision_link(old_instance_uuid, src_uri, dst_uri, src_ipv6, dst_ipv6, 
                      new_bandwidth, alias=""):
     """Reprovision a SENSE link"""
+    time.sleep(120)
     return uuid.uuid4()
