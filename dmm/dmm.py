9555906ddbb8ff6a5b4fd94c1e02fba3b2055ed7
Jonathan Guiang <jkguiang@gmail.com>
2022-03-29 22:19:52 -0700

    added executable

--- dmm/dmm.py
+++ dmm/dmm.py
@@ -1,7 +1,4 @@
-import argparse
-import sys
 import yaml
-import signal
 import logging
 from multiprocessing.connection import Listener
 from dmm.site import Site
@@ -210,44 +207,3 @@ class DMM:
 
         if len(link_close_jobs) > 0:
             self.update_links("adjusting for request deletion")
-
-def sigint_handler(dmm):
-    def actual_handler(sig, frame):
-        logging.info("Stopping DMM (received SIGINT)")
-        dmm.stop()
-        sys.exit(0)
-    return actual_handler
-
-if __name__ == "__main__":
-    cli = argparse.ArgumentParser(description="Rucio-SENSE data movement manager")
-    cli.add_argument(
-        "-n", "--n_workers", type=int, default=4, 
-        help="maximum number of worker processes"
-    )
-    cli.add_argument(
-        "--loglevel", type=str, default="WARNING", 
-        help="log level: DEBUG, INFO, WARNING (default), or ERROR"
-    )
-    cli.add_argument(
-        "--logfile", type=str, default="dmm.log", 
-        help="path to log file (default: ./dmm.log)"
-    )
-    args = cli.parse_args()
-
-    # Set up logging handlers
-    handlers = [logging.FileHandler(filename=args.logfile)]
-    if args.loglevel.upper() == "DEBUG":
-        handlers.append(logging.StreamHandler(sys.stdout))
-    # Configure logging
-    logging.basicConfig(
-        format="(%(threadName)s) [%(asctime)s] %(levelname)s: %(message)s",
-        datefmt="%m-%d-%Y %H:%M:%S %p",
-        level=getattr(logging, args.loglevel.upper()),
-        handlers=handlers
-    )
-
-    # Start DMM
-    dmm = DMM(n_workers=args.n_workers)
-    signal.signal(signal.SIGINT, sigint_handler(dmm))
-    logging.info("Starting DMM")
-    dmm.start()
