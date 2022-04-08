1d035ab6dbc5696f3caf0f86ff12036aaab79f27
Jonathan Guiang <jkguiang@gmail.com>
2022-04-08 10:57:08 -0700

    merged link and request objects

--- dmm/dmm.py
+++ dmm/dmm.py
@@ -2,7 +2,6 @@ import yaml
 import logging
 from multiprocessing.connection import Listener
 from dmm.site import Site
-from dmm.link import Link
 from dmm.request import Request
 from dmm.orchestrator import Orchestrator
 
@@ -20,15 +19,12 @@ class DMM:
         with open(authkey_file, "rb") as f_in:
             self.authkey = f_in.read()
 
-    def __get_request_id(self, rule_id, src_rse_name, dst_rse_name):
-        return f"{rule_id}_{src_rse_name}_{dst_rse_name}"
-
     def __dump(self):
-        for request, link in self.requests.values():
+        for request in self.requests.values():
             logging.debug(
                 f"{request} | "
-                f"{link.src_site.rse_name} --> {link.dst_site.rse_name} "
-                f"{link.bandwidth} Mb/s"
+                f"{request.src_site.rse_name} --> {request.dst_site.rse_name} "
+                f"{request.bandwidth} Mb/s"
             )
 
     def stop(self):
@@ -53,40 +49,40 @@ class DMM:
                     self.finisher_handler(payload)
 
     @staticmethod
-    def link_updater(request_id, link, new_bandwidth, msg, monitoring):
-        logging.debug(f"{request_id} | {link.bandwidth} --> {new_bandwidth}; {msg}")
-        if link.is_open:
-            link.reprovision(new_bandwidth)
+    def link_updater(request, msg, monitoring):
+        # Update link
+        old_bandwidth = request.bandwidth
+        if request.link_is_open:
+            request.reprovision_link()
+            changed = (old_bandwidth != request.bandwidth)
         else:
-            link.bandwidth = new_bandwidth
-            link.open()
-
-        link.update_history(msg, monitoring=monitoring)
+            request.open_link()
+            changed = True
+        # Update metadata
+        if changed and not request.best_effort:
+            logging.debug(f"{request} | {old_bandwidth} --> {request.bandwidth}; {msg}")
+        request.update_history(msg, monitoring=monitoring)
 
     @staticmethod
-    def link_closer(request_id, link, monitoring):
-        logging.debug(f"{request_id} | closing link")
-        link.close()
-        link.update_history("closing link", monitoring=monitoring)
+    def link_closer(request, monitoring):
+        logging.debug(f"{request} | closing link")
+        request.close_link()
+        request.update_history("closing link", monitoring=monitoring)
         # Log the promised and actual bandwidths
-        summary = link.get_summary(string=True, monitoring=monitoring)
-        logging.info(f"{request_id} | {summary}; closed")
+        summary = request.get_summary(string=True, monitoring=monitoring)
+        logging.info(f"{request} | {summary}; closed")
 
-    def update_links(self, msg):
+    def update_requests(self, msg):
         """Update bandwidth provisions for all links"""
         logging.info("updating link bandwidth provisions and metadata")
-        for request_id, (request, link) in self.requests.items():
-            new_bandwidth = link.get_max_bandwidth()*request.get_bandwidth_fraction()
-            if not link.is_open or link.bandwidth != new_bandwidth:
-                # Submit SENSE query
-                link_updater_args = (
-                    request_id,
-                    link,
-                    new_bandwidth,
-                    msg if link.is_open else "opened link",
-                    self.monitoring
-                )
-                self.orchestrator.put(request_id, DMM.link_updater, link_updater_args)
+        for request_id, request in self.requests.items():
+            # Submit SENSE query
+            link_updater_args = (
+                request,
+                msg if request.link_is_open else "opened link",
+                self.monitoring
+            )
+            self.orchestrator.put(request_id, DMM.link_updater, link_updater_args)
 
     def preparer_handler(self, payload):
         """
@@ -113,7 +109,7 @@ class DMM:
             for rse_pair_id, request_attr in prepared_rule.items():
                 src_rse_name, dst_rse_name = rse_pair_id.split("&")
                 # Check if request has already been processed
-                request_id = self.__get_request_id(rule_id, src_rse_name, dst_rse_name)
+                request_id = Request.id(rule_id, src_rse_name, dst_rse_name)
                 if request_id in self.requests.keys():
                     logging.error("request ID already processed--should never happen!")
                     continue
@@ -126,15 +122,12 @@ class DMM:
                 if dst_rse_name not in self.sites.keys():
                     self.sites[dst_rse_name] = dst_site
                 # Create new Request
-                req = Request(request_id, rule_id, src_site, dst_site, **request_attr)
-                req.register()
-                # Create new Link
-                link = Link(src_site, dst_site, best_effort=(req.priority == 0))
-                link.register()
+                request = Request(rule_id, src_site, dst_site, **request_attr)
+                request.register()
                 # Store new request and its corresponding link
-                self.requests[req.request_id] = (req, link)
+                self.requests[request_id] = request
 
-        self.update_links("accommodating for new requests")
+        self.update_requests("accommodating for new requests")
 
     def submitter_handler(self, payload):
         """
@@ -161,22 +154,22 @@ class DMM:
             for rse_pair_id, report in submitter_reports.items():
                 # Get request
                 src_rse_name, dst_rse_name = rse_pair_id.split("&")
-                request_id = self.__get_request_id(rule_id, src_rse_name, dst_rse_name)
-                request, link = self.requests[request_id]
+                request_id = Request.id(rule_id, src_rse_name, dst_rse_name)
+                req = self.requests[request_id]
                 # Update request
-                request.n_transfers_submitted += report["n_transfers_submitted"]
-                if report["priority"] != request.priority:
-                    request.priority = report["priority"]
+                req.n_transfers_submitted += report["n_transfers_submitted"]
+                if report["priority"] != req.priority:
+                    req.priority = report["priority"]
                     n_priority_changes += 1
                 # Get SENSE link endpoints
                 sense_map[rule_id][rse_pair_id] = {
                     # block_to_ipv6 translation is a hack; should not be needed in the future
-                    request.src_site.rse_name: link.src_site.block_to_ipv6[link.src_ipv6],
-                    request.dst_site.rse_name: link.dst_site.block_to_ipv6[link.dst_ipv6]
+                    req.src_site.rse_name: req.src_site.block_to_ipv6[req.src_ipv6],
+                    req.dst_site.rse_name: req.dst_site.block_to_ipv6[req.dst_ipv6]
                 }
 
         if n_priority_changes > 0:
-            self.update_links("adjusting for priority update")
+            self.update_requests("adjusting for priority update")
 
         return sense_map
 
@@ -204,22 +197,20 @@ class DMM:
             for rse_pair_id, report in finisher_reports.items():
                 # Get request
                 src_rse_name, dst_rse_name = rse_pair_id.split("&")
-                request_id = self.__get_request_id(rule_id, src_rse_name, dst_rse_name)
-                request, link = self.requests[request_id]
+                request_id = Request.id(rule_id, src_rse_name, dst_rse_name)
+                request = self.requests[request_id]
                 # Update request
                 request.n_transfers_finished += report["n_transfers_finished"]
                 request.n_bytes_transferred += report["n_bytes_transferred"]
                 if request.n_transfers_finished == request.n_transfers_total:
                     # Stage the link for closure
-                    link.deregister()
-                    closer_args = (request_id, link, self.monitoring)
+                    request.deregister()
+                    closer_args = (request, self.monitoring)
                     self.orchestrator.clear(request_id)
                     self.orchestrator.put(request_id, DMM.link_closer, closer_args)
                     n_link_closures += 1
-                    # Deregister the request
-                    request.deregister()
                     # Clean up
                     self.requests.pop(request_id)
 
         if n_link_closures > 0:
-            self.update_links("adjusting for request deletion")
+            self.update_requests("adjusting for request deletion")
--- dmm/request.py
+++ dmm/request.py
@@ -1,11 +1,17 @@
+import time
+import dmm.nonsense_api as sense_api
+from dmm.monitoring import PrometheusSession
+
 class Request:
-    def __init__(self, request_id, rule_id, src_site, dst_site, transfer_ids, priority, 
+    def __init__(self, rule_id, src_site, dst_site, transfer_ids, priority, 
                  n_bytes_total, n_transfers_total):
-        self.request_id = request_id
+        # General attributes
+        self.request_id = Request.id(rule_id, src_site.rse_name, dst_site.rse_name)
         self.rule_id = rule_id
         self.src_site = src_site # DMM Site object
         self.dst_site = dst_site # DMM Site object
-        # Unpacked from prepared_request["attr"]
+
+        # Attributes unpacked from prepared_request["attr"]
         self.transfer_ids = transfer_ids
         self.priority = priority
         self.n_bytes_total = n_bytes_total
@@ -14,8 +20,101 @@ class Request:
         self.n_transfers_submitted = 0
         self.n_transfers_finished = 0
 
+        # SENSE link attributes
+        self.best_effort = (self.priority == 0)
+        self.link_is_open = False
+        self.src_ipv6 = ""
+        self.dst_ipv6 = ""
+        self.bandwidth = 0
+        self.history = [(time.time(), self.bandwidth, 0, "init")]
+        self.prometheus = PrometheusSession()
+        self.sense_link_id = sense_api.get_profile_uuid()
+        self.theoretical_bandwidth = -1
+        self.update_theoretical_bandwidth()
+
+    @staticmethod
+    def id(rule_id, src_rse_name, dst_rse_name):
+        return f"{rule_id}_{src_rse_name}_{dst_rse_name}"
+
     def __str__(self):
         return f"Request({self.request_id})"
+
+    def update_history(self, msg, monitoring=False):
+        """Track the promised and actual bandwidth"""
+        time_last, _, _, _ = self.history[-1]
+        time_now = time.time()
+        if monitoring:
+            actual_bandwidth = self.prometheus.get_average_throughput(
+                self.src_ipv6,
+                self.src_site.rse_name,
+                time_last,
+                time_now
+            )
+        else:
+            actual_bandwidth = -1
+
+        self.history.append((time_now, self.bandwidth, actual_bandwidth, msg))
+
+    def get_summary(self, string=False, monitoring=False):
+        """Return the average promised and actual bandwidth"""
+        times, promised_bw, actual_bw, _ = zip(*self.history)
+        dts = [t - times[t_i] for t_i, t in enumerate(times[1:])]
+        avg_promise = sum([bw*dt for bw, dt in zip(promised_bw, dts)])/sum(dts)
+        if monitoring:
+            avg_actual = self.prometheus.get_average_throughput(
+                self.src_site.block_to_ipv6[self.src_ipv6], # block_to_ipv6 is a temporary hack
+                self.src_site.rse_name,
+                times[1], # times[1] is when the link is actually provisioned
+                times[-1]
+            )
+        else:
+            avg_actual = -1
+        if string:
+            return f"{avg_promise:0.1f}, {avg_actual:0.1f} (promised, actual bandwidth [Mb/s])"
+        else:
+            return avg_promise, avg_actual
+
+    def register(self):
+        """Register new request at the source and destination sites"""
+        self.src_site.add_request(self.dst_site.rse_name, self.priority)
+        self.dst_site.add_request(self.src_site.rse_name, self.priority)
+        if self.best_effort:
+            self.src_ipv6 = self.src_site.default_ipv6
+            self.dst_ipv6 = self.dst_site.default_ipv6
+        else:
+            self.src_ipv6 = self.src_site.reserve_ipv6()
+            self.dst_ipv6 = self.dst_site.reserve_ipv6()
+
+    def deregister(self):
+        """Deregister new request at the source and destination sites"""
+        self.src_site.remove_request(self.dst_site.rse_name, self.priority)
+        self.dst_site.remove_request(self.src_site.rse_name, self.priority)
+        if not self.best_effort:
+            self.src_site.free_ipv6(self.src_ipv6)
+            self.dst_site.free_ipv6(self.dst_ipv6)
+        self.src_ipv6 = ""
+        self.dst_ipv6 = ""
+
+    def update_theoretical_bandwidth(self):
+        if self.best_effort:
+            return
+        link_id, theoretical_bandwidth = sense_api.get_theoretical_bandwidth(
+            self.src_site.sense_name,
+            self.dst_site.sense_name,
+            instance_uuid=self.sense_link_id
+        )
+        self.sense_link_id = link_id
+        self.theoretical_bandwidth = theoretical_bandwidth
+
+    def get_max_bandwidth(self):
+        if self.best_effort:
+            return 0
+        else:
+            return min(
+                self.src_site.get_uplink_provision(self.dst_site.rse_name),
+                self.dst_site.get_uplink_provision(self.src_site.rse_name),
+                self.theoretical_bandwidth
+            )
 
     def get_bandwidth_fraction(self):
         """Return bandwidth fraction
@@ -24,22 +123,44 @@ class Request:
         fraction = ----------------------------------------------
                    sum(all priorities between my source and dest)
         """
-        return self.priority/self.src_site.prio_sums.get(self.dst_site.rse_name)
+        if self.best_effort:
+            return 0
+        else:
+            return self.priority/self.src_site.prio_sums.get(self.dst_site.rse_name)
 
-    def same_site_pair(self, other_request):
-        """Return if another request involves the same sites as this one"""
-        same_src = self.src_site == other_request.src_site
-        same_dst = self.dst_site == other_request.dst_site
-        inverted_src = self.src_site == other_request.dst_site
-        inverted_dst = self.dst_site == other_request.src_site
-        return (same_src and same_dst) or (inverted_src and inverted_dst)
+    def reprovision_link(self):
+        """Reprovision SENSE link"""
+        old_bandwidth = self.bandwidth
+        new_bandwidth = self.get_max_bandwidth()*self.get_bandwidth_fraction()
+        if not self.best_effort and new_bandwidth != old_bandwidth:
+            # Update SENSE link; note: in the future, this should not change the link ID
+            self.sense_link_id = sense_api.reprovision_link(
+                self.sense_link_id, 
+                self.src_site.sense_name,
+                self.dst_site.sense_name,
+                self.src_ipv6,
+                self.dst_ipv6,
+                new_bandwidth
+            )
+            self.bandwidth = new_bandwidth
 
-    def register(self):
-        """Register new request at the source and destination sites"""
-        self.src_site.add_request(self.dst_site.rse_name, self.priority)
-        self.dst_site.add_request(self.src_site.rse_name, self.priority)
+    def open_link(self):
+        """Create SENSE link"""
+        if not self.best_effort:
+            self.bandwidth = self.get_max_bandwidth()*self.get_bandwidth_fraction()
+            self.sense_link_id = sense_api.create_link(
+                self.src_site.sense_name,
+                self.dst_site.sense_name,
+                self.src_ipv6,
+                self.dst_ipv6,
+                self.bandwidth,
+                instance_uuid=self.sense_link_id
+            )
+        self.link_is_open = True
 
-    def deregister(self):
-        """Deregister new request at the source and destination sites"""
-        self.src_site.remove_request(self.dst_site.rse_name, self.priority)
-        self.dst_site.remove_request(self.src_site.rse_name, self.priority)
+    def close_link(self):
+        """Close SENSE link"""
+        if not self.best_effort:
+            sense_api.delete_link(self.sense_link_id)
+            self.sense_link_id = ""
+        self.link_is_open = False
