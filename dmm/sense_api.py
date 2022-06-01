2a24a3496b41019482d00e93aeabf4e1d15e0e70
Jonathan Guiang <jkguiang@gmail.com>
2022-06-01 11:43:19 -0700

    added logging

--- dmm/sense_api.py
+++ dmm/sense_api.py
@@ -1,6 +1,7 @@
 import json
 import yaml
 import re
+import logging
 from sense.client.workflow_combined_api import WorkflowCombinedApi
 from sense.client.profile_api import ProfileApi
 from sense.client.discover_api import DiscoverApi
@@ -20,13 +21,10 @@ def good_response(response):
     return len(response) > 0 and "ERROR" not in response and "error" not in response
 
 def get_ipv6_pool(uri):
-    """Return a list of IPv6 subnets at given site
-
-    Note: not fully supported by SENSE yet
-    """
+    """Return a list of IPv6 subnets at given site"""
     discover_api = DiscoverApi()
     response = discover_api.discover_domain_id_ipv6pool_get(uri)
-    print(response)
+    logging.debug(response)
     if len(response) == 0 or "ERROR" in response:
         raise ValueError(f"Discover query failed for {uri}")
     else:
@@ -34,28 +32,27 @@ def get_ipv6_pool(uri):
         return response["routing"][0]["ipv6_subnet_pool"].split(",")
 
 def get_uplink_capacity(uri):
-    """Return the maximum uplink capacity in Mb/s for a given site
-
-    Notes: not fully supported by SENSE yet
-    """
+    """Return the maximum uplink capacity in Mb/s for a given site"""
     discover_api = DiscoverApi()
     response = discover_api.discover_domain_id_peers_get(uri)
-    print(response)
+    logging.debug(response)
     if not good_response(response):
         raise ValueError(f"Discover query failed for {uri}")
     else:
         response = json.loads(response)
         return float(response["peer_points"][0]["port_capacity"])
 
-def get_uri(rse_name, regex=".*?", full=True):
+def get_uri(rse_name, regex=".*?", full=False):
     """Return the SENSE URI for a given Rucio RSE"""
     discover_api = DiscoverApi()
     response = discover_api.discover_lookup_name_get(rse_name, search="NetworkAddress")
-    print(response)
+    logging.debug(response)
     if not good_response(response):
         raise ValueError(f"Discover query failed for {rse_name}")
     else:
         response = json.loads(response)
+        if not response["results"]:
+            raise ValueError(f"No results for '{rse_name}'")
         matched_results = []
         for result in response["results"]:
             if re.search(regex, result["name/tag/value"]):
@@ -72,11 +69,11 @@ def get_uri(rse_name, regex=".*?", full=True):
 def __get_rooturi(full_uri):
     """Return the root SENSE URI for a given full SENSE URI"""
     discover_api = DiscoverApi()
-    uri = discover_api.discover_lookup_rooturi_get(full_uri)
-    if not good_response(uri):
+    root_uri = discover_api.discover_lookup_rooturi_get(full_uri)
+    if not good_response(root_uri):
         raise ValueError(f"Discover query failed for {full_uri}")
     else:
-        return uri
+        return root_uri
 
 def stage_link(src_uri, dst_uri, src_ipv6, dst_ipv6, instance_uuid="", alias=""):
     """Return the maximum theoretical bandwidth available between two sites
@@ -113,7 +110,7 @@ def stage_link(src_uri, dst_uri, src_ipv6, dst_ipv6, instance_uuid="", alias="")
         intent["alias"] = alias
     # Query SENSE and extract theoretical bandwidth from its response
     response = workflow_api.instance_create(json.dumps(intent))
-    print(response)
+    logging.debug(response)
     if not good_response(response):
         raise ValueError(f"SENSE query failed for {instance_uuid}")
     else:
@@ -131,7 +128,7 @@ def provision_link(instance_uuid, src_uri, dst_uri, src_ipv6, dst_ipv6, bandwidt
     workflow_api = WorkflowCombinedApi()
     workflow_api.si_uuid = instance_uuid
     # Modify service instance
-    print(f"instance uuid: {instance_uuid}")
+    logging.debug(f"instance uuid: {instance_uuid}")
     intent = {
         "service_profile_uuid": get_profile_uuid(),
         "queries": [
@@ -154,7 +151,7 @@ def provision_link(instance_uuid, src_uri, dst_uri, src_ipv6, dst_ipv6, bandwidt
         intent["alias"] = alias
     # Push intent JSON to SENSE
     response = workflow_api.instance_create(json.dumps(intent))
-    print(response)
+    logging.debug(response)
     if not good_response(response):
         raise ValueError(f"SENSE query failed for {instance_uuid}")
     else:
@@ -165,7 +162,7 @@ def delete_link(instance_uuid):
     """Delete a SENSE link"""
     workflow_api = WorkflowCombinedApi()
     status = workflow_api.instance_get_status(si_uuid=instance_uuid)
-    print(status)
+    logging.debug(status)
     if "error" in status:
         raise ValueError(status)
     if "CREATE" not in status and "REINSTATE" not in status and "MODIFY" not in status:
@@ -177,7 +174,7 @@ def delete_link(instance_uuid):
         force=str("READY" not in status).lower()
     )
     status = workflow_api.instance_get_status(si_uuid=instance_uuid)
-    print(status)
+    logging.debug(status)
     if "CANCEL - READY" in status:
         workflow_api.instance_delete(si_uuid=instance_uuid)
     else:
